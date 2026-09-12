728672ea482204177e6f7c54476f7bc3ab714782
smaratech <srm1835@g.rit.edu>
2026-09-12 14:07:07 -0700

    chore: initialize Spring Boot project

---
+++ src/main/java/com/surabhimarathe/interfaceautomation/InterfaceaiAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.surabhimarathe.interfaceautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceaiAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceaiAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surabhimarathe/interfaceautomation/InterfaceaiAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surabhimarathe.interfaceautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceaiAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
